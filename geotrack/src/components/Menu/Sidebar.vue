95688301edc5d4f75cc66b0a7c3ff25b3dd29aaf
aoliveira <andreluiz1088@hotmail.com>
2024-11-09 12:06:51 -0300

    fix: enabling query when just drawing circle

--- geotrack/src/components/Menu/Sidebar.vue
+++ geotrack/src/components/Menu/Sidebar.vue
@@ -45,7 +45,7 @@
         title="Filtro de Áreas Geográficas" color="primary"></v-btn>
 
       <!-- Botão para Sair -->
-      <v-btn key="map-marker" icon="mdi-export" @click="handleLogout" title="Saída" color="primary"></v-btn>
+      <v-btn key="map-marker" icon="mdi-export" @click="handleLogout" title="Saída" color="#F44336"></v-btn>
     </v-speed-dial>
   </div>
 </template>
