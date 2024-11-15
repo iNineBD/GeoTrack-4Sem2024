dba86590498c3ff14f380e4275c0c80ff9d1c501
AlitaAmancio <amancioalita@gmail.com>
2024-11-14 21:30:47 -0300

    feat: adding "Dúvidas?" button

---
+++ geotrack/src/components/Info/InfoPanel.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <v-btn
+      class="tooltip-btn"
+      title="Dúvidas?"
+      color="primary"
+      icon="mdi-help-circle"
+    >
+    </v-btn>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+.tooltip-btn {
+  width: 20px;
+  height: 20px;
+  border-radius: 50%;
+  z-index: 10;
+}
+</style>
