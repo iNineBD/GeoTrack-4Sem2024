56c91630d2665b2851d9e6e9be33aa20123d9662
AlitaAmancio <amancioalita@gmail.com>
2024-11-22 23:38:53 -0300

    feat: add Áreas Geográficas info

---
+++ geotrack/src/components/Info/GeographicAreasModal.vue
@@ -0,0 +1,192 @@
+<template>
+  <div class="modal-content">
+    <h1 class="modal-title">Áreas Geográficas</h1>
+
+    <!-- Página 1 -->
+    <div v-if="currentPage === 1">
+      <p>
+        Sejam bem-vindos ao GeoTrack! Essa página está destinada a solucionar
+        possíveis dúvidas sobre como utilizar nossa aplicação. A imagem abaixo é
+        referente ao filtro de Áreas Geográficas, onde você pode buscar os
+        Pontos de Parada de um Usuário dentro de uma área específica. Avance
+        para a próxima tela para entender melhor como funciona!
+      </p>
+      <div class="image-container">
+        <img
+          src="../../assets/infoImgs/GeographicAreasModal/AGInterface.png"
+          alt="Menu Áreas Geográficas"
+        />
+        <p class="image-caption">Menu Áreas Geográficas</p>
+      </div>
+    </div>
+
+    <!-- Página 2 -->
+    <div v-else-if="currentPage === 2">
+      <p>
+        Para criar uma nova Área Geográfica, clique no ícone de mais (+) e
+        desenhe um círculo no mapa. Após verificar os detalhes, clique em
+        "Salvar" para registrar a área criada.
+      </p>
+      <div class="image-grid">
+        <div class="image-item">
+          <img
+            src="../../assets/infoImgs/GeographicAreasModal/AGCriarArea.png"
+            alt="Criação de Área Geográfica"
+          />
+          <p class="image-caption">Exemplo de Criação de Área Geográfica</p>
+        </div>
+        <div class="image-item">
+          <img
+            src="../../assets/infoImgs/GeographicAreasModal/AGSalvarZona.png"
+            alt="Salvando Área Geográfica"
+          />
+          <p class="image-caption">Salvando Área Geográfica</p>
+        </div>
+      </div>
+    </div>
+
+    <!-- Página 3 -->
+    <div v-else-if="currentPage === 3">
+      <p>
+        Quando você seleciona uma das Áreas Geográficas ela aparecerá no mapa,
+        clicando sobre ela você verá opções como "Editar" e "Deletar".
+      </p>
+      <div class="image-grid">
+        <div class="image-item">
+          <img
+            src="../../assets/infoImgs/GeographicAreasModal/AGSelecionarZona.png"
+            alt="Selecionando Área Geográfica"
+          />
+          <p class="image-caption">Selecionando Área Geográfica</p>
+        </div>
+        <div class="image-item">
+          <img
+            src="../../assets/infoImgs/GeographicAreasModal/AGDisplayZona.png"
+            alt="Área Geográfica em Display"
+          />
+          <p class="image-caption">Área Geográfica em Display</p>
+        </div>
+      </div>
+    </div>
+
+    <!-- Página 4 -->
+    <div v-else-if="currentPage === 4">
+      <p>
+        Com a Área Geográfica ativa, selecione um usuário e use os filtros
+        rápidos de data ou o calendário para escolher um período de até 30 dias.
+        Após isso, clique em "Consultar" e aguarde os resultados.
+      </p>
+      <div class="image-grid">
+        <div class="image-item">
+          <img
+            src="../../assets/infoImgs/GeographicAreasModal/AGSelecionarUsuario.png"
+            alt="Selecionar Usuário"
+          />
+          <p class="image-caption">Selecionar Usuário</p>
+        </div>
+        <div class="image-item">
+          <img
+            src="../../assets/infoImgs/GeographicAreasModal/AGSelecionarData.png"
+            alt="Selecionar Data"
+          />
+          <p class="image-caption">Selecionar Data</p>
+        </div>
+      </div>
+    </div>
+
+    <!-- Página 5 -->
+    <div v-else-if="currentPage === 5">
+      <p>
+        Aqui você vê o resultado da consulta. Os Pontos de Parada são exibidos
+        no mapa, e abaixo do menu, você encontrará os detalhes, incluindo o nome
+        do usuário, identificação do dispositivo e todos os pontos encontrados
+        no período e área selecionados.
+      </p>
+      <div class="image-container">
+        <img
+          src="../../assets/infoImgs/GeographicAreasModal/AGDados.png"
+          alt="Dados na Tela"
+        />
+        <p class="image-caption">Dados na Tela</p>
+      </div>
+    </div>
+
+    <!-- Navegação -->
+    <div class="modal-navigation">
+      <v-btn
+        :disabled="currentPage === 1"
+        @click="currentPage--"
+        class="nav-btn"
+      >
+        Anterior
+      </v-btn>
+      <v-btn
+        :disabled="currentPage === totalPages"
+        @click="currentPage++"
+        class="nav-btn"
+      >
+        Próximo
+      </v-btn>
+    </div>
+
+    <!-- Botão de fechar -->
+    <v-btn @click="$emit('close')">Fechar</v-btn>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+
+const currentPage = ref(1);
+const totalPages = 5;
+</script>
+
+<style scoped>
+.modal-content {
+  background: rgba(255, 255, 255, 0.9);
+  padding: 20px;
+  border-radius: 10px;
+  text-align: center;
+}
+.modal-title {
+  font-size: 24px;
+  margin-bottom: 16px;
+  color: #333;
+}
+.modal-navigation {
+  display: flex;
+  justify-content: space-between;
+  margin: 20px 0;
+}
+.nav-btn {
+  margin: 0 10px;
+}
+p {
+  font-size: 18px;
+  line-height: 1.6;
+  color: #444;
+}
+.image-grid {
+  display: flex;
+  justify-content: space-around;
+  gap: 20px;
+  margin-top: 20px;
+}
+.image-item {
+  text-align: center;
+}
+.image-container img,
+.image-item img {
+  width: 90%;
+  max-width: 600px;
+  max-height: 400px;
+  height: auto;
+  border-radius: 8px;
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+}
+.image-caption {
+  font-size: 16px;
+  color: #666;
+  margin-top: 8px;
+}
+</style>
---
+++ geotrack/src/components/Info/StopPointsModal.vue
@@ -0,0 +1,154 @@
+<template>
+  <div class="modal-content">
+    <!-- Página 1 -->
+    <div v-if="currentPage === 1">
+      <h1 class="modal-title">Pontos de Parada</h1>
+      <p>
+        Sejam bem-vindos ao GeoTrack! Essa página está destinada a solucionar
+        possíveis dúvidas sobre como utilizar nossa aplicação. A imagem abaixo é
+        referente ao filtro de Pontos de Parada, onde você pode buscar os Pontos
+        de Parada de até cinco usuários diferentes dentro de um período de
+        trinta dias. Avance para a próxima tela para entender melhor como
+        funciona!
+      </p>
+      <div class="image-container">
+        <img
+          src="../../assets/infoImgs/StopPointsModal/interfaceClean.jpg"
+          alt="Menu Pontos de Parada"
+        />
+        <p class="image-caption">Menu Pontos de Parada</p>
+      </div>
+    </div>
+
+    <!-- Página 2 -->
+    <div v-else-if="currentPage === 2">
+      <h1 class="modal-title">Pontos de Parada</h1>
+      <p>
+        Para iniciar a busca, selecione até cinco usuários. Em seguida, use os
+        filtros de data ou escolha um período de até 30 dias no calendário.
+        Depois, clique em "Consultar" e aguarde os resultados.
+      </p>
+      <div class="image-grid">
+        <div class="image-item">
+          <p class="image-caption">Selecionar Usuários</p>
+          <img
+            src="../../assets/infoImgs/StopPointsModal/selecionarUsuarios.png"
+            alt="Selecionar Usuários"
+          />
+        </div>
+        <div class="image-item">
+          <p class="image-caption">Selecionar Data</p>
+          <img
+            src="../../assets/infoImgs/StopPointsModal/selecionarData.png"
+            alt="Selecionar Data"
+          />
+        </div>
+      </div>
+    </div>
+
+    <!-- Página 3 -->
+    <div v-else-if="currentPage === 3">
+      <h1 class="modal-title">Pontos de Parada</h1>
+      <p>
+        Aqui você vê o resultado da consulta. Os Pontos de Parada são exibidos
+        no mapa, e abaixo do menu, você encontrará os detalhes, incluindo o nome
+        do usuário, identificação do dispositivo e todos os pontos encontrados
+        no período selecionado.
+      </p>
+      <div class="image-grid">
+        <div class="image-item">
+          <p class="image-caption">Dados na Tela</p>
+          <img
+            src="../../assets/infoImgs/StopPointsModal/dadosNaTela.png"
+            alt="Dados na Tela"
+          />
+        </div>
+        <div class="image-item">
+          <p class="image-caption">Informações Pontos de Parada</p>
+          <img
+            src="../../assets/infoImgs/StopPointsModal/informacoesPontosParada.png"
+            alt="Informações Pontos de Parada"
+          />
+        </div>
+      </div>
+    </div>
+
+    <!-- Botões de navegação -->
+    <div class="modal-navigation">
+      <v-btn
+        :disabled="currentPage === 1"
+        @click="currentPage--"
+        class="nav-btn"
+      >
+        Anterior
+      </v-btn>
+      <v-btn
+        :disabled="currentPage === totalPages"
+        @click="currentPage++"
+        class="nav-btn"
+      >
+        Próximo
+      </v-btn>
+    </div>
+
+    <!-- Botão de fechar -->
+    <v-btn @click="$emit('close')">Fechar</v-btn>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+
+const currentPage = ref(1);
+const totalPages = 3;
+</script>
+
+<style scoped>
+.modal-content {
+  background: rgba(255, 255, 255, 0.9);
+  padding: 20px;
+  border-radius: 10px;
+  text-align: center;
+}
+.modal-title {
+  font-size: 24px;
+  margin-bottom: 16px;
+  color: #333;
+}
+.modal-navigation {
+  display: flex;
+  justify-content: space-between;
+  margin: 20px 0;
+}
+.nav-btn {
+  margin: 0 10px;
+}
+p {
+  font-size: 18px;
+  line-height: 1.6;
+  color: #444;
+}
+.image-grid {
+  display: flex;
+  justify-content: space-around;
+  gap: 20px;
+  margin-top: 20px;
+}
+.image-item {
+  text-align: center;
+}
+.image-container img,
+.image-item img {
+  width: 90%;
+  max-width: 600px;
+  max-height: 400px;
+  height: auto;
+  border-radius: 8px;
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+}
+.image-caption {
+  font-size: 16px;
+  color: #666;
+  margin-top: 8px;
+}
+</style>
